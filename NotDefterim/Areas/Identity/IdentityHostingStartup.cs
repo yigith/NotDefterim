4fdfcb271e1693f2ab356cd1036fc4d69ad1624b
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-09-07 09:58:05 +0300

    ana sayfa, identity scaffold

---
+++ NotDefterim/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotDefterim.Data;
+
+[assembly: HostingStartup(typeof(NotDefterim.Areas.Identity.IdentityHostingStartup))]
+namespace NotDefterim.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
