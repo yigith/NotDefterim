3482fd40a0df00f04255defe665e713ee0d2ce37
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-09-07 13:04:21 +0300

    not ekleme

--- NotDefterim/Controllers/HomeController.cs
+++ NotDefterim/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NotDefterim.Data;
 using NotDefterim.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NotDefterim.Controllers
@@ -12,15 +15,25 @@ namespace NotDefterim.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            this.db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // giriş yapmış kullanıcı yok ise
+            if (!User.Identity.IsAuthenticated)
+                return View("IndexAnonymous");
+
+
+            // giriş yapmış kullanıcının id'si
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            return View(db.Notes.Where(n => n.AuthorId == userId).ToList());
         }
 
         public IActionResult Privacy()
