a093f072d3db8c819d36aa4bbe3654c48c6ddc5f
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-09-09 11:15:19 +0300

    seed roles and users

---
+++ NotDefterim/Data/ApplicationDbSeed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDefterim.Data
+{
+    public static class ApplicationDbSeed
+    {
+        public static async Task SeedRolesAndUsers(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            // admin rolü henüz yoksa oluştur
+            if (!await roleManager.RoleExistsAsync("admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole() { Name = "admin" });
+            }
+
+            // admin@example.com kullanıcısı yoksa oluştur ve admin rolüne ata
+            if (!await userManager.Users.AnyAsync(x => x.Email == "admin@example.com"))
+            {
+                ApplicationUser adminUser = new ApplicationUser()
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(adminUser, "P@ssword1");
+                await userManager.AddToRoleAsync(adminUser, "admin");
+            }
+        }
+    }
+}
--- NotDefterim/Program.cs
+++ NotDefterim/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NotDefterim.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,18 @@ namespace NotDefterim
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                await ApplicationDbSeed.SeedRolesAndUsers(roleManager, userManager);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
