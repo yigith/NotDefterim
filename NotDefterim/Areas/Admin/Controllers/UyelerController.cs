0fb1e1a010e10e7efc74dca8c154cce38bc94140
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-09-13 13:33:23 +0300

    üye durum yönetimi

---
+++ NotDefterim/Areas/Admin/Controllers/UyelerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotDefterim.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDefterim.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "admin")]
+    public class UyelerController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public UyelerController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult DurumGuncelle(string kullaniciId, bool aktifMi)
+        {
+            ApplicationUser user = db.Users.Find(kullaniciId);
+
+            if (user == null)
+                return NotFound();
+
+            user.LockoutEnabled = true;
+            user.LockoutEnd = aktifMi ? null : DateTimeOffset.MaxValue;
+            db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
