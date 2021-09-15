7bbb834dadf6dda37f67bccf42367a9fc764d021
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-09-15 13:30:05 +0300

    profil resmi view component

---
+++ NotDefterim/Models/ProfilResmiComponentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDefterim.Models
+{
+    public class ProfilResmiComponentViewModel
+    {
+        public string DosyaAd { get; set; }
+
+        public int Genislik { get; set; }
+
+        public int Yukseklik { get; set; }
+
+        public string Sinif { get; set; }
+    }
+}
---
+++ NotDefterim/ViewComponents/ProfilResmiViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotDefterim.Data;
+using NotDefterim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NotDefterim.ViewComponents
+{
+    // https://docs.microsoft.com/tr-tr/aspnet/core/mvc/views/view-components?view=aspnetcore-5.0
+    public class ProfilResmiViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfilResmiViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int genislik, int yukseklik, string sinif)
+        {
+            var userId = ((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+            var vm = new ProfilResmiComponentViewModel()
+            {
+                DosyaAd = user.Photo,
+                Genislik = genislik,
+                Yukseklik = yukseklik,
+                Sinif = sinif
+            };
+            return View(vm);
+        }
+    }
+}
